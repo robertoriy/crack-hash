fdd886296ae006b33d07a57e4d38dcb75ddeb18c
robertoriy <holdenxhume@gmail.com>
2024-03-29 12:35:17 +0700

    add beans for executors and map

--- manager/src/main/java/ru/nsu/robertoriy/manager/configuration/ManagerConfig.java
+++ manager/src/main/java/ru/nsu/robertoriy/manager/configuration/ManagerConfig.java
@@ -3,16 +3,29 @@ package ru.nsu.robertoriy.manager.configuration;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import ru.nsu.robertoriy.manager.dto.response.StatusResponse;
 
 @Configuration
-public class BeanConfig {
+public class ManagerConfig {
     @Bean
     @Scope("prototype")
     public Map<UUID, StatusResponse> getStatusResponseMap() {
+        return new ConcurrentHashMap<>();
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public ScheduledExecutorService getScheduler() {
+        return Executors.newSingleThreadScheduledExecutor();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Map<UUID, Integer> getCounterMap() {
         return new ConcurrentHashMap<>();
     }
 }
--- worker/src/main/java/ru/nsu/robertoriy/worker/configuration/WorkerConfig.java
+++ worker/src/main/java/ru/nsu/robertoriy/worker/configuration/WorkerConfig.java
@@ -1,13 +1,13 @@
 package ru.nsu.robertoriy.worker.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WorkerConfig {
-    @Bean
+    @Bean(destroyMethod = "shutdown")
     public ExecutorService getExecutorService() {
         return Executors.newCachedThreadPool();
     }
