f2a026781ebb6488adabc912c50da9957a886f56
robertoriy <holdenxhume@gmail.com>
2024-04-29 08:16:07 +0700

    update config validation

--- manager/src/main/java/ru/nsu/robertoriy/manager/configuration/ApplicationConfig.java
+++ manager/src/main/java/ru/nsu/robertoriy/manager/configuration/ApplicationConfig.java
@@ -2,6 +2,7 @@ package ru.nsu.robertoriy.manager.configuration;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -12,7 +13,7 @@ public record ApplicationConfig(
     @Max(value = 9, message = "Part Count must be less than 10")
     int partCount,
 
-    String workerUrl,
+    @NotBlank String workerUrl,
 
     @Min(value = 10, message = "Max word length must be greater than 10 seconds")
     @Max(value = 300, message = "Part Count must be less than 5 minutes")
